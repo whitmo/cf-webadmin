f4a22914a64e9d06b60ce4ce1ef9e0b6504af7e1
Whit Morriss <whit.morriss@canonical.com>
2014-10-23 12:02:50 -0500

    start uaac action

--- hooks/actions.py
+++ hooks/actions.py
@@ -1,10 +1,17 @@
 from charmhelpers.core import hookenv
+from cloudfoundry import contexts
 from path import path
 import yaml
 
 
 def log_start(service_name):
+    uaacxt = contexts.UAARelation()
     hookenv.log('cf-webadmin starting')
+
+
+def setup_uaac_client(name):
+
+    pass
 
 
 def render_webadmin_config(vars,
